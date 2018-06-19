34dc387a5de0aea2539e9ff3d2b15889db06e4c8
Carib Rivera-Guerra <caribfguerra@gmail.com>
2018-06-19 15:37:07 -0400

    Edit ifs

--- 3.rb
+++ 3.rb
@@ -27,8 +27,7 @@ def change_possibilities(amount, nums)
       if sum + nums[j] <= amount
         sum += nums[j]
         hold << nums[j]
-      end
-      if sum + nums[j] > amount
+      else
         if jcount < nums.size
           j -= 1
           jcount += 1
@@ -47,9 +46,9 @@ def change_possibilities(amount, nums)
   tally.keys.size
 end
 
-p change_possibilities(3, [3, 2, 1]) === 3
-p change_possibilities(4, [1, 2, 3]) === 4
-p change_possibilities(6, [1, 2, 3]) === 7
+# p change_possibilities(3, [3, 2, 1]) #=== 3
+# p change_possibilities(4, [1, 2, 3]) #=== 4
+p change_possibilities(6, [1, 2, 3]) #=== 7
 
 ### 3 ###
 # 3
@@ -70,3 +69,36 @@ p change_possibilities(6, [1, 2, 3]) === 7
 # 2, 2, 1, 1
 # 2, 1, 1, 1, 1
 # 1, 1, 1, 1, 1, 1
+
+#
+# def change_possibilities(amount, nums)
+#   tally = {}
+#   i = 0
+#   while i < nums.size
+#     sum = nums[i]
+#     hold = [nums[i]]
+#     j = 0
+#     jcount = 0
+#     while j < nums.size
+#       if sum + nums[j] <= amount
+#         sum += nums[j]
+#         hold << nums[j]
+#       end
+#       if sum + nums[j] > amount
+#         if jcount < nums.size
+#           j -= 1
+#           jcount += 1
+#         else
+#           j += 1
+#         end
+#         if sum === amount
+#           tally[hold.sort.map(&:to_s).join] = sum
+#           sum = nums[i]
+#           hold = [nums[i]]
+#         end
+#       end
+#       i += 1 if j >= nums.size
+#     end
+#   end
+#   tally.keys.size
+# end
